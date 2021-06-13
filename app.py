7a771983dceaad442dc083e4b2d5bd1271e3afb0
Ignacio Arriagada <iarriagada@gemini.edu>
2021-06-13 17:22:31 -0400

    Application is now receiving/displaying/refreshing values from 2 epics channels

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import epics
 import os
 import time
 
 
 os.environ["EPICS_CA_ADDR_LIST"] = "172.17.2.36"
-app = Flask(__name__)
 tcs_lst = epics.PV("tcs:LST")
 time.sleep(0.200)
+print(tcs_lst.value)
+tcs_hb = epics.PV("tcs:heartbeat")
+time.sleep(0.200)
+print(tcs_hb.value)
+app = Flask(__name__)
 test = 0
 
 @app.route("/")
 def home():
     global test
     test += 1
-    value2 = tcs_lst.value
+    lst_val = tcs_lst.value
+    hb_val = tcs_hb.value
     # value = 'hola'
-    print(f"this is the value: {value2}")
-    return render_template("home.html", value=value2, test=test)
-    # return render_template("home.html", value=update_lst(), test=test)
+    print(f"this is the lst: {lst_val}")
+    print(f"this is the heartbeat: {hb_val}")
+    return render_template("home.html",
+                           value_lst=lst_val,
+                           value_hb=hb_val,
+                           test=test)
 
-@app.route("/lst")
-def update_lst():
-    value = tcs_lst.value
-    # value = 'hola'
-    print(f"this is the value: {value}")
-    return value
+@app.route("/epics")
+def update_epics():
+    lst_val = tcs_lst.value
+    hb_val = tcs_hb.value
+    print(f"this is the lst: {lst_val}")
+    print(f"this is the heartbeat: {hb_val}")
+    values = {
+        'value_lst':lst_val,
+        'value_hb':hb_val
+    }
+    return jsonify(values)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8888)
-    # pass
 
